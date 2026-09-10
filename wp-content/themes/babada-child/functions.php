2c0955a121b3d1a9d561e6399c4ef6267b448832
fharahap85 <fharahap85@gmail.com>
2026-09-10 17:00:10 +0700

    chore: setup Babada child theme assets

--- wp-content/themes/babada-child/functions.php
+++ wp-content/themes/babada-child/functions.php
@@ -6,25 +6,30 @@ if (!defined('ABSPATH')) {
 
 function babada_child_enqueue_assets()
 {
+    $theme_version = wp_get_theme()->get('Version');
+
+    $css_file = get_stylesheet_directory() . '/assets/css/custom.css';
+    $js_file  = get_stylesheet_directory() . '/assets/js/custom.js';
+
     wp_enqueue_style(
         'babada-child-style',
         get_stylesheet_uri(),
         [],
-        wp_get_theme()->get('Version')
+        $theme_version
     );
 
     wp_enqueue_style(
         'babada-custom-style',
         get_stylesheet_directory_uri() . '/assets/css/custom.css',
         ['babada-child-style'],
-        '1.0.0'
+        file_exists($css_file) ? filemtime($css_file) : $theme_version
     );
 
     wp_enqueue_script(
         'babada-custom-script',
         get_stylesheet_directory_uri() . '/assets/js/custom.js',
         [],
-        '1.0.0',
+        file_exists($js_file) ? filemtime($js_file) : $theme_version,
         true
     );
 }
